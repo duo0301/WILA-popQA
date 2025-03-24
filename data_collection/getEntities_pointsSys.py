350ce4bbb83e92d2f187b69481c9d7ec0043854a
MIFERROUDJENE <mouloud.iferroudjene@emse.fr>
2025-03-24 14:45:33 +0100

    supporting qendpoint querying

--- data_collection/getEntities_pointsSys.py
+++ data_collection/getEntities_pointsSys.py
@@ -5,16 +5,20 @@ from tqdm import tqdm
 import random
 import pandas as pd
 
-
-def query_wikidata(sparql_query):
+def query_wikidata(sparql_query, add_prefix=True):
     # endpoint_url = "https://query.wikidata.org/sparql"
     endpoint_url = "http://localhost:1234/api/endpoint/sparql"
+    
     sparql = SPARQLWrapper(endpoint_url)
-    sparql.setQuery(f"""
+    if add_prefix:
+        sparql.setQuery(f"""
         PREFIX wd: <http://www.wikidata.org/entity/>
         PREFIX wdt: <http://www.wikidata.org/prop/direct/>
         {sparql_query}
-    """)
+        """)
+    else:
+        sparql.setQuery(sparql_query)
+
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     return results
@@ -72,98 +76,132 @@ def build_select_query(values_clause):
         }}
     """
 
+def construct_sparql_query_get_creators(languages_clause, subclasses_clause, limit=1000, offset=0):
+    sparql_query = f"""
+    SELECT distinct ?creator 
+    WHERE {{
+        VALUES ?languages_spoken {{ {languages_clause} }}
+        VALUES ?occupation {{ {subclasses_clause} }}
+        ?creator wdt:P31 wd:Q5; # Instance of human
+            wdt:P106 ?occupation; # Occupation
+            wdt:P1412 ?languages_spoken .
+    }}
+    LIMIT {limit} OFFSET {offset}
+    """
+    return sparql_query
 
-# Step 1: Set the list of occupations interested in
-# Creator (Q2500638), politician (Q82955), actor (Q33999), writer (Q36180)
-occupation_classes = ["Q2500638", "Q82955", "Q33999", "Q36180"]   
+if __name__ == '__main__':
+
+    # Step 1: Set the list of occupations interested in
+    # Creator (Q2500638), politician (Q82955), actor (Q33999), writer (Q36180)
+    occupation_classes = ["Q2500638", "Q82955", "Q33999", "Q36180"]   
+
+    # Define language sets
+    language_sets = {
+        # "English": ["Q1860", "Q7976", "Q7979", "Q44676", "Q44679", "Q7053766", "Q48767245"],  # English variants
+        "Arabic": ["Q13955", "Q29919", "Q56499","Q1194795", "Q1654327","Q5329979"],  # Arabic and Egyptian Arabic
+        # "German": ["Q188", "Q248682", "Q306626","Q106937689", "Q26721", "Q387066"],  # German and its variants
+        # "French": ["Q150", "Q1450506","Q214086", "Q3083193", "Q979914","Q83503"],  # French and Canadian French
+        # "Kannada": ["Q33673", "Q6363888","Q6478506"],
+        # "Italian": ["Q652"],
+        # "Polish": ["Q809"],
+        # "Hindi": ["Q1568"],
+        # "Russian": ["Q7737","Q608923"],
+        # "Chinese": ["Q7850"]
+    }
 
 
-# Define language sets
-language_sets = {
-    #"English": ["Q1860", "Q7976", "Q7979", "Q44676", "Q44679", "Q7053766", "Q48767245"],  # English variants
-    "Arabic": ["Q13955", "Q29919", "Q56499","Q1194795", "Q1654327","Q5329979"],  # Arabic and Egyptian Arabic
-    #"German": ["Q188", "Q248682", "Q306626","Q106937689", "Q26721", "Q387066"],  # German and its variants
-    #"French": ["Q150", "Q1450506","Q214086", "Q3083193", "Q979914","Q83503"],  # French and Canadian French
-    #"Kannada": ["Q33673", "Q6363888","Q6478506"],
-    #"Italian": ["Q652"],
-    #"Polish": ["Q809"],
-    #"Hindi": ["Q1568"],
-    #"Russian": ["Q7737","Q608923"],
-    #"Chinese": ["Q7850"]
-}
-
-for offset in range(10000, 10000*2, 1000):
     # Step 2: Retrieve creator IDs for each language group
+    output_dir = os.path.join(os.path.dirname(__file__), 'data','dataset_v2')
+    
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "entities_ids"), exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "entities_properties"), exist_ok=True)
+
     for language_group, language_ids in language_sets.items():
         print(f"Step 2: Retrieving creator IDs for language group: {language_group}")
-        languages_clause = " ".join([f"wd:{lang_id}" for lang_id in language_ids])
-        subclasses_clause = " ".join([f"wd:{class_id}" for class_id in occupation_classes])
+        entity_ids_file_path = os.path.join(output_dir, "entities_ids", f'entity_ids_{language_group}.txt')
+        entity_properties_file_path = os.path.join(output_dir, "entities_properties", f'entities_properties_{language_group}.csv')
+        entity_properties_matrix_file_path = os.path.join(output_dir, "entities_properties_matrix", f'{language_group}.csv')
+        if os.path.exists(entity_ids_file_path):
+            print(f"File with Entities IDs already exists for language group {language_group}: {entity_ids_file_path}")
+            with open(entity_ids_file_path, 'r') as file:
+                set_creators_ids = set(line.strip() for line in file)
+                print(f"Total entities IDs retrieved for language group {language_group}: {len(set_creators_ids)}")
+        else:
+            # Build the SPARQL query
+            languages_clause = " ".join([f"wd:{lang_id}" for lang_id in language_ids])
+            subclasses_clause = " ".join([f"wd:{class_id}" for class_id in occupation_classes])
+            limit = 10000
+            total = 200000
+            set_creators_ids = set()
 
-        sparql_query = f"""
-            PREFIX wd: <http://www.wikidata.org/entity/>
-            PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-            SELECT distinct ?creator WHERE {{
-            ?creator wdt:P31 wd:Q5;  # Instance of human
-                    wdt:P106 ?occupation;  # Occupation
-                    wdt:P1412 ?languages_spoken.
-            VALUES ?languages_spoken {{ {languages_clause} }}
-            VALUES ?occupation {{ {subclasses_clause} }}
-            }}
-            LIMIT 1000 OFFSET {offset}
-        """
-        try:
-            creators = query_wikidata(sparql_query)
-            if not creators['results']['bindings']:
-                print(f"No creators found for language group {language_group}")
-                continue
-            creator_ids = [result['creator']['value'].split('/')[-1] for result in creators['results']['bindings']]
-            print(f"Retrieved {len(creator_ids)} creators for language group {language_group}")
-        except Exception as e:
-            print(f"Error retrieving creators for language group {language_group}: {e}")
-            continue
+            for offset in tqdm(range(0, total, limit), desc=f"Retrieving creators for {language_group}"):
+                
+                try:
+                    sparql_query = construct_sparql_query_get_creators(languages_clause, subclasses_clause, limit, offset)
+                    query_result = query_wikidata(sparql_query, add_prefix=True)
+                    if not query_result['results']['bindings']:
+                        print(f"No creators found for language group {language_group}")
+                        print(f"Total creators retrieved for language group {language_group}: {len(set_creators_ids)}")
+                        break
+                    else : 
+                        creator_ids_batch = [result['creator']['value'].split('/')[-1] for result in query_result['results']['bindings']]
+                        set_creators_ids.update(set(creator_ids_batch))
+                        print(f"Retrieved {len(creator_ids_batch)} creators for language group {language_group}")
+                        print(f"Total creators retrieved for language group {language_group}: {len(set_creators_ids)}")
 
-        filename = f'entity_ids_{language_group}.txt'
-        filename_prop = f'entities_properties_{language_group}.csv'
-
-        if os.path.exists(filename_prop):
-            # Read QIDs from the existing file
-            data = pd.read_csv(filename_prop)
-            existing_creator_ids = set(data['creator_id'])
-            print(f"Found {len(existing_creator_ids)} existing entity IDs for language group {language_group} in {filename_prop}")
-            print(f"Query returned {len(creator_ids)} new entity IDs for language group {language_group}")
-            # Remove the existing creator IDs from the list
-            creator_ids = list(set(creator_ids) - existing_creator_ids)
-            print(f"Adding {len(creator_ids)} new creator IDs for language group {language_group}")
-
-        if os.path.exists(filename):
-            # with open(filename, 'r') as file:
-                # existing_creator_ids = {line.strip() for line in file}
-                # Add the new creator IDs
-            full_creator_ids = list(set(creator_ids).union(existing_creator_ids))
-            # Writ creator IDs to the file
-            with open(filename, 'w') as file:
-                for creator_id in full_creator_ids:
+                except Exception as e:
+                    print(f"Error retrieving creators for language group {language_group}: {e}")
+                    print(f"Total creators retrieved for language group {language_group}: {len(set_creators_ids)}")
+                    break
+            
+            with open(entity_ids_file_path, 'w') as file:
+                for creator_id in list(set_creators_ids):
                     file.write(f"{creator_id}\n")
 
+        # if os.path.exists(entity_properties_file_path):
+        #     # Read QIDs from the existing file
+        #     data = pd.read_csv(entity_properties_file_path)
+        #     existing_creator_ids = set(data['creator_id'])
+        #     print(f"Found {len(existing_creator_ids)} existing entity IDs for language group {language_group} in {filename_prop}")
+        #     print(f"Query returned {len(creator_ids)} new entity IDs for language group {language_group}")
+        #     # Remove the existing creator IDs from the list
+        #     creator_ids = list(set(creator_ids) - existing_creator_ids)
+        #     print(f"Adding {len(creator_ids)} new creator IDs for language group {language_group}")
+        # else:
+        #     existing_creator_ids = []
+
+        # if os.path.exists(entity_ids_file_path):
+        #     full_creator_ids = list(set(creator_ids).union(existing_creator_ids))
+        # else : 
+        #     full_creator_ids = creator_ids
+
+        # Step 3: Filtering IDs for additional properties
 
         filtered_creator_ids = [] 
         data = []
-        # Step 3: Filtering IDs for additional properties
-        batch_size = 150  # Number of creators to check per batch
+        
+        batch_size = 10  # Number of creators to check per batch
 
         i = 0
+        creator_ids = list (set_creators_ids)
         nb_creator_ids = len(creator_ids)
-        with tqdm(total=nb_creator_ids, desc=f"Filtering creators in batches for {language_group}") as pbar:
+        with tqdm(total=nb_creator_ids, 
+                  desc=f"Filtering creators in batches for {language_group}",
+                  unit="entity") as pbar:
 
             while i < nb_creator_ids :
                 batch = creator_ids[i:i + batch_size]
                 values_clause = " ".join([f"wd:{creator_id}" for creator_id in batch])
                 select_query = build_select_query(values_clause)
+                
                 retries = 4
                 for attempt in range(retries):
 
                     try:
-                        batch_filtered_creators = query_wikidata(select_query)
+                        batch_filtered_creators = query_wikidata(select_query, add_prefix=True)
                         for result in batch_filtered_creators['results']['bindings']:
                             creator_id = result['creator']['value'].split('/')[-1]
                             data.append({
@@ -191,7 +229,7 @@ for offset in range(10000, 10000*2, 1000):
                                 'has_religion': int(result['has_religion']['value']),
                                 'has_ethnic_group': int(result['has_ethnic_group']['value'])
                             })
-                        time.sleep(1 + random.uniform(0, 2))  # Add delay to avoid overwhelming the server
+                        # time.sleep(1 + random.uniform(0, 2))  # Add delay to avoid overwhelming the server
                         i += batch_size
                         pbar.update(batch_size)
                         break
@@ -207,20 +245,20 @@ for offset in range(10000, 10000*2, 1000):
                             time.sleep(10)
                             if i > 0:
                                 i -= batch_size
-                    except (ValueError, KeyError) as e:
-                        print(f"Error on attempt {attempt + 1} for batch {batch[0]}: {e}")
-                        time.sleep(5)  # Add a small delay between retries
                     if attempt == retries - 1:
                         print(f"Skipping batch {batch[0]} after {retries} attempts")
-        
-        
-        if os.path.exists(filename_prop):
-            # Read the existing file into a DataFrame
-            df = pd.read_csv(filename_prop)
-            # Append the new data
-            df = pd.concat([df, pd.DataFrame(data)]).drop_duplicates().reset_index(drop=True)
-        else :
-            # Convert the data list to a pandas DataFrame
-            df = pd.DataFrame(data)
-        # Save the DataFrame to a CSV file for further analysis
-        df.to_csv(f'entities_properties_{language_group}.csv', index=False)
+            
+        # Save the properties data
+        properties_matrix = pd.DataFrame(data)
+        properties_matrix.to_csv(entity_properties_matrix_file_path, index=False)
+            
+            # if os.path.exists(filename_prop):
+            #     # Read the existing file into a DataFrame
+            #     df = pd.read_csv(filename_prop)
+            #     # Append the new data
+            #     df = pd.concat([df, pd.DataFrame(data)]).drop_duplicates().reset_index(drop=True)
+            # else :
+            #     # Convert the data list to a pandas DataFrame
+            #     df = pd.DataFrame(data)
+            # # Save the DataFrame to a CSV file for further analysis
+            # df.to_csv(f'entities_properties_{language_group}.csv', index=False)
